d07fd38466886a45dbe34b95a266360a3c8a55ec
Shrutika <shrutikaphy1728@gmail.com>
2026-09-05 02:26:49 +0530

    Refactor convolution function to use 4-way unrolling

--- task1/src/conv_unroll.cpp
+++ task1/src/conv_unroll.cpp
@@ -1,19 +1,16 @@
 // conv_unroll.cpp  STAGE 2: LOOP UNROLLING
 #include "convolution.h"
 
-void conv_unroll(const float* in, float* out, const float* ker,
-                 int H, int W, int K) {
+void conv_naive(const float* in, float* out, const float* ker,
+                int H, int W, int K) {
     const int p = K / 2;
     const int in_stride = W + 2 * p;
 
     for (int oy = 0; oy < H; ++oy) {
         int ox = 0;
-
-        // Unroll by 8
-        for (; ox + 7 < W; ox += 8) {
+        // Process 4 output pixels per iteration
+        for (; ox + 3 < W; ox += 4) {
             float acc0 = 0.0f, acc1 = 0.0f, acc2 = 0.0f, acc3 = 0.0f;
-            float acc4 = 0.0f, acc5 = 0.0f, acc6 = 0.0f, acc7 = 0.0f;
-
             for (int ky = 0; ky < K; ++ky) {
                 const float* in_row = in + (oy + ky) * in_stride;
                 const float* k_row  = ker + ky * K;
@@ -23,23 +20,14 @@ void conv_unroll(const float* in, float* out, const float* ker,
                     acc1 += in_row[ox + 1 + kx] * w;
                     acc2 += in_row[ox + 2 + kx] * w;
                     acc3 += in_row[ox + 3 + kx] * w;
-                    acc4 += in_row[ox + 4 + kx] * w;
-                    acc5 += in_row[ox + 5 + kx] * w;
-                    acc6 += in_row[ox + 6 + kx] * w;
-                    acc7 += in_row[ox + 7 + kx] * w;
                 }
             }
             out[oy * W + ox + 0] = acc0;
             out[oy * W + ox + 1] = acc1;
             out[oy * W + ox + 2] = acc2;
             out[oy * W + ox + 3] = acc3;
-            out[oy * W + ox + 4] = acc4;
-            out[oy * W + ox + 5] = acc5;
-            out[oy * W + ox + 6] = acc6;
-            out[oy * W + ox + 7] = acc7;
         }
-
-        // Remainder loop for any leftover pixels
+        // Remainder (W not divisible by 4)
         for (; ox < W; ++ox) {
             float acc = 0.0f;
             for (int ky = 0; ky < K; ++ky)
@@ -49,4 +37,3 @@ void conv_unroll(const float* in, float* out, const float* ker,
         }
     }
 }
-
